6ae1c3b637c38e6d53fbc7cdffec8612a19e3afa
Christian Riedel <riechr@it-profi-hamburg.de>
2021-06-16 11:32:00 +0200

    Create first version of start page with App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,16 @@
-function App() {
-  return (
-    <div className="App">
-      <h1>MyChargepoint</h1>
-    </div>
-  );
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>MyChargepoint</h2>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
